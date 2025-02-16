d4a39e667baa41b6f6c7db1274d43089ee0ea83b
Boston343 <branden.austin1@gmail.com>
2025-02-16 07:57:53 -0500

    chore: prettier formatting

--- src/config/navData.json.ts
+++ src/config/navData.json.ts
@@ -1,51 +1,51 @@
 export interface navLinkItem {
-  text: string;
-  link: string;
-  newTab?: boolean; // adds target="_blank" rel="noopener noreferrer" to link
+	text: string;
+	link: string;
+	newTab?: boolean; // adds target="_blank" rel="noopener noreferrer" to link
 }
 
 export interface navDropdownItem {
-  text: string;
-  dropdown: navLinkItem[];
+	text: string;
+	dropdown: navLinkItem[];
 }
 
 export type navItem = navLinkItem | navDropdownItem;
 
 // note: 1 level of dropdown is supported
 const navConfig: navItem[] = [
-  {
-    text: "Home",
-    link: "/",
-  },
-  {
-    text: "portfolios",
-    link: "/portfolio",
-  },
-  {
-    text: "Pricing",
-    link: "/#pricing",
-  },
-  {
-    text: "Pages",
-    dropdown: [
-      {
-        text: "Portfolio",
-        link: "/portfolio/couple-1",
-      },
-      {
-        text: "Legal",
-        link: "/privacy-policy/",
-      },
-      {
-        text: "Elements",
-        link: "/elements/",
-      },
-      {
-        text: "404",
-        link: "/not-a-link/",
-      },
-    ],
-  },
+	{
+		text: "Home",
+		link: "/",
+	},
+	{
+		text: "portfolios",
+		link: "/portfolio",
+	},
+	{
+		text: "Pricing",
+		link: "/#pricing",
+	},
+	{
+		text: "Pages",
+		dropdown: [
+			{
+				text: "Portfolio",
+				link: "/portfolio/couple-1",
+			},
+			{
+				text: "Legal",
+				link: "/privacy-policy/",
+			},
+			{
+				text: "Elements",
+				link: "/elements/",
+			},
+			{
+				text: "404",
+				link: "/not-a-link/",
+			},
+		],
+	},
 ];
 
 export default navConfig;
--- src/js/textUtils.ts
+++ src/js/textUtils.ts
@@ -4,14 +4,14 @@
  * @param text: string - text to slugify
  */
 export function slugify(text: string): string {
-  return text
-    .toString()
-    .toLowerCase() // convert to lowercase
-    .replace(/\s+/g, "-") // replace spaces with -
-    .replace(/[^\w-]+/g, "") // remove all non-word chars
-    .replace(/--+/g, "-") // replace multiple dashes with single dash
-    .replace(/^-+/, "") // trim dash from start of text
-    .replace(/-+$/, ""); // trim dash from end of text
+	return text
+		.toString()
+		.toLowerCase() // convert to lowercase
+		.replace(/\s+/g, "-") // replace spaces with -
+		.replace(/[^\w-]+/g, "") // remove all non-word chars
+		.replace(/--+/g, "-") // replace multiple dashes with single dash
+		.replace(/^-+/, "") // trim dash from start of text
+		.replace(/-+$/, ""); // trim dash from end of text
 }
 
 /**
@@ -19,17 +19,17 @@ export function slugify(text: string): string {
  * @param text: string - text to humanize
  */
 export function humanize(text: string): string {
-  const slugifiedText = slugify(text);
-  return (
-    slugifiedText
-      .replace(/-/g, " ") // replace "-" with space
-      // .toLowerCase();
-      .replace(
-        // upper case first letter of every word, and lower case the rest
-        /\w\S*/g,
-        (w) => w.charAt(0).toUpperCase() + w.slice(1).toLowerCase(),
-      )
-  );
+	const slugifiedText = slugify(text);
+	return (
+		slugifiedText
+			.replace(/-/g, " ") // replace "-" with space
+			// .toLowerCase();
+			.replace(
+				// upper case first letter of every word, and lower case the rest
+				/\w\S*/g,
+				(w) => w.charAt(0).toUpperCase() + w.slice(1).toLowerCase(),
+			)
+	);
 }
 
 // --------------------------------------------------------
@@ -38,10 +38,10 @@ export function humanize(text: string): string {
  * @param date: string | number | Date - date to format
  */
 export function formatDate(date: string | number | Date): string {
-  return new Date(date).toLocaleDateString("en-US", {
-    timeZone: "UTC",
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-  });
+	return new Date(date).toLocaleDateString("en-US", {
+		timeZone: "UTC",
+		year: "numeric",
+		month: "short",
+		day: "numeric",
+	});
 }
